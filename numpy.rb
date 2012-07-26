8e9a6fc634bd0174057dbb4b240b2f95a40389b8
samueljohn <github@samueljohn.de>
2012-07-26 18:07:44 +0200

    numpy: 1.6.2

--- numpy.rb
+++ numpy.rb
@@ -1,17 +1,15 @@
 require 'formula'
 
 class Numpy < Formula
-  url 'http://sourceforge.net/projects/numpy/files/NumPy/1.6.1/numpy-1.6.1.tar.gz'
+  url 'http://sourceforge.net/projects/numpy/files/NumPy/1.6.2/numpy-1.6.2.tar.gz'
   homepage 'http://www.scipy.org/'
-  md5 '2bce18c08fc4fce461656f0f4dd9103e'
+  sha1 'c36c471f44cf914abdf37137d158bf3ffa460141'
   head 'https://github.com/numpy/numpy.git'
 
   depends_on 'nose' => :python
 
   def install
     ENV.fortran
-    ENV['CC']='gcc-4.2'
-    ENV['CXX']='g++-4.2'
     ENV['FFLAGS']='-ff2c'
 
     system "python", "setup.py", "build", "--fcompiler=gfortran"
