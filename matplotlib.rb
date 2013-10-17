003bec9d06cc137605231e245066a04cbc0e0b97
Samuel John <github@SamuelJohn.de>
2013-10-17 20:03:33 +0200

    matplotlib: python3 support and audit fixes
    - Also experimental gtk support.

--- matplotlib.rb
+++ matplotlib.rb
@@ -22,7 +22,7 @@ class Matplotlib < Formula
   head 'https://github.com/matplotlib/matplotlib.git'
 
   depends_on :python
-  # depends_on :python3 => :optional # todo ...
+  depends_on :python3 => :optional
   depends_on :freetype
   depends_on :libpng
   depends_on 'numpy'
@@ -31,8 +31,8 @@ class Matplotlib < Formula
   depends_on 'ghostscript' => :optional
   depends_on 'pyside' => :optional
   depends_on 'pyqt' => :optional
-  # depends_on 'pygtk' => :optional
-  # depends_on 'pygobject' if build.with? 'pygtk'
+  depends_on 'pygtk' => :optional
+  depends_on 'pygobject' if build.with? 'pygtk'
   # On Xcode-only Macs, the Tk headers are not found by matplotlib
   depends_on 'homebrew/dupes/tcl-tk' => :optional
   depends_on :python => 'pyparsing'
@@ -54,16 +54,16 @@ class Matplotlib < Formula
     # This block will take care that "python" is the right python version and
     # will be run once for each python executable.
     python do
-      system python.binary, "setup.py", "install", "--prefix=#{prefix}", "--record=installed.txt", "--single-version-externally-managed"
+      system python, "setup.py", "install", "--prefix=#{prefix}", "--record=installed.txt", "--single-version-externally-managed"
     end
   end
 
   def caveats
-    python.standard_caveats +
-    <<-EOS.undent
+    s = <<-EOS.undent
       If you want to use the `wxagg` backend, do `brew install wxwidgets`.
       This can be done even after the matplotlib install.
     EOS
+    s += python.standard_caveats if python
   end
 
   test do
