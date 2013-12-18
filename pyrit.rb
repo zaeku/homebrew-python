7e910b3830dd30f5aff744e8bbe90cbb082c188e
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-17 19:10:13 -0600

    pyrit 0.4.0 (new formula)

---
+++ pyrit.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Pyrit < Formula
+  homepage 'https://code.google.com/p/pyrit/'
+  url 'https://pyrit.googlecode.com/files/pyrit-0.4.0.tar.gz'
+  sha1 '3041c0d593c21df88f41416be872eca0997cf22b'
+
+  depends_on :python
+  depends_on 'libdnet' => 'with-python'
+  depends_on 'scapy'
+
+  def install
+      system "python", "setup.py", "build"
+      system "python", "setup.py", "install", "--prefix=#{prefix}"
+  end
+end
