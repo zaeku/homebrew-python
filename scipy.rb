93c28cd455a8eb2f63bf376a4a78edbed3e2e349
Arjun Singh <arjun810@gmail.com>
2013-12-20 14:57:07 -0800

    scipy: flag to fix compilation with non-Apple gcc.

--- scipy.rb
+++ scipy.rb
@@ -53,6 +53,7 @@ class Scipy < Formula
 
     # The Accelerate.framework uses a g77 ABI
     ENV.append 'FFLAGS', '-ff2c' if build.without? 'openblas'
+    ENV.append 'CPPFLAGS', '-D__ACCELERATE__' unless build.with? 'openblas'
 
     rm_f 'site.cfg' if build.devel?
     Pathname('site.cfg').write config
