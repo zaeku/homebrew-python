4d062cecce1a382c5e4e43770c9cb7a0ea9a3a5b
Stefano Zanella <zanella.stefano@gmail.com>
2014-02-20 16:38:35 +0100

    scapy: add pycap dependency
    On Mavericks, opening scapy without pypcap installed results in an error
    message. While is seems not fatal, it looks better to me to fix the
    issue.
    
    Closes #94.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- scapy.rb
+++ scapy.rb
@@ -7,6 +7,7 @@ class Scapy < Formula
 
   depends_on :python
   depends_on 'libdnet' => 'with-python'
+  depends_on 'pypcap'
 
   def install
     system "python", 'setup.py', 'install', "--prefix=#{prefix}"
